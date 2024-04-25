ca93237ec20bc9a817cac6081b35feecbe242900
vishal <vanagire@gmail.com>
2024-04-25 12:00:01 +0530

    my first commit from vishal pc

---
+++ variables_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+readonly name="Paul"
+
+age=35
+
+city="Pune"
+
+
+echo "Hello friends, my name is $name and age is $age and my city is $city"
+
+name="Sachin"
+
+echo "my name is $name"
+
+HOSTNAME=$(hostname)
+
+KERNEL=$(uname)
+
+echo "name of the host is $HOSTNAME and OS is $KERNEL"
